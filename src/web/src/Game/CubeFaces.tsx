508a8f5cc317070654f6b59922a3337920eff301
mbostwick <mbostwick89@gmail.com>
2023-12-02 20:45:41 -0600

    Pushing up WIP..

--- src/web/src/Game/CubeFaces.tsx
+++ src/web/src/Game/CubeFaces.tsx
@@ -5,9 +5,10 @@ export function CubeFaces(props: {rows: number, cols: number, faces: number}) {
     const numberOfItems = props.cols * props.rows * props.faces;
     const items: player[] = Array.from(Array(numberOfItems));
     const getPositionForItem = (position: number): Vector3 => {
-        const row= 0;
-        const col = 0;
-        const face = 0;
+        const space = 2;
+        const row= Math.floor(position/ props.rows) + (position % props.rows) * space;
+        const col = Math.floor(position/ props.cols) + (position % props.cols) * space;
+        const face = Math.floor(position/ props.faces) + (position % props.faces) * space;
 
         return new Vector3(row, col, face);
     }
