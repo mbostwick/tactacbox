5e5bc1b9ac01626b5d7a1287f27c830282c941c1
mbostwick89 <mbostwick89@gmail.com>
2023-12-03 02:12:26 -0600

    Revert "Pushing Up Breaking Code."
    This reverts commit 9d02a525513ada8dca39b2bb12b14ef960f166eb.

--- src/web/src/Game/Game.tsx
+++ src/web/src/Game/Game.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import {CubeFaces} from "./CubeFaces";
-import {CameraControls} from "@react-three/drei";
+import {CameraControls} from "../Vendor/CameraControls";
 
 export function NewGame(): ActiveGame {
     return {
